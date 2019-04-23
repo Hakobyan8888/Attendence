66d9a268202030ca7f5e2067f589459ee91088b9
StepanHokobyan <31405336+StepanHokobyan@users.noreply.github.com>
2019-04-23 17:24:24 +0400

    The final Attendence

---
+++ Attendence/AddUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attendence
+{
+    class AddUser
+    {
+        public List<string> userName { get; set; } = new List<string>();
+
+        public delegate void NamesAreOverlaped(string user);
+        public event NamesAreOverlaped OnOverlap;
+        public event NamesAreOverlaped OnNoneOverlap;
+
+        public void AddingUser()
+        {
+            Console.WriteLine("Enter UserName");
+            userName.Add(Console.ReadLine());
+            CheckUser();
+        }
+
+        public void CheckUser()
+        {
+            switch (userName[userName.Count - 1])
+            {
+                case "Jack":
+                    this.OnOverlap(userName[userName.Count - 1]);
+                    break;
+                case "Steven":
+                    this.OnOverlap(userName[userName.Count - 1]);
+                    break;
+                case "Mathew":
+                    this.OnOverlap(userName[userName.Count - 1]);
+                    break;
+                default:
+                    if (OnNoneOverlap != null)
+                    {
+                        this.OnNoneOverlap(userName[userName.Count - 1]);
+                    }
+                    break;
+            }
+            
+        }
+    }
+}
--- Attendence/Program.cs
+++ Attendence/Program.cs
@@ -10,6 +10,24 @@ namespace Attendence
     {
         static void Main(string[] args)
         {
+            AddUser addUser = new AddUser();
+            addUser.OnOverlap += AddUser_OnOverlap;
+            addUser.OnNoneOverlap += AddUser_OnNoneOverlap;
+            Console.WriteLine(@"For Adding a user please enter 'add' ");
+            if(Console.ReadLine() == "add")
+            {
+                addUser.AddingUser();
+            }
+        }
+
+        private static void AddUser_OnNoneOverlap(string userName)
+        {
+            Console.WriteLine($"Welcome {userName}");
+        }
+
+        private static void AddUser_OnOverlap(string userName)
+        {
+            Console.WriteLine($"You entered {userName}. Please enter another name");
         }
     }
 }
